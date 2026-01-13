89eaaf7c084eb16763fa438601ae2dcedb21505f
LogLInk1K <exitreflex@gmail.com>
2026-01-13 18:00:23 +0800

    feat(ui): 增强导航和交互元素的视觉反馈效果
    为HeaderLink、ThemeToggle、Header等组件添加悬停动画效果
    更新网站标题为中文并优化滚动计数器的样式

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Log - 1k.ink';
+export const SITE_TITLE = '波罗歌 - 醉后不知天在水';
 export const SITE_DESCRIPTION = '波罗歌的幻想杂谈';
