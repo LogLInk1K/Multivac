ae62a1d2bd2f4e1e9a341f98dce510bc03a46e9f
LogLInk1K <exitreflex@gmail.com>
2026-02-19 21:15:09 +0800

    feat(SEO): 添加robots.txt和增强文章页面的SEO元数据
    添加robots.txt路由以支持搜索引擎爬取
    在BaseHead组件中增加文章发布时间、修改时间等SEO相关元数据
    添加JSON-LD结构化数据以提升搜索引擎对文章的理解

---
+++ src/pages/robots.txt.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = ({ site }) => {
+	const robotsTxt = `User-agent: *
+Allow: /
+
+Sitemap: ${site}sitemap-index.xml
+`;
+
+	return new Response(robotsTxt, {
+		headers: {
+			'Content-Type': 'text/plain; charset=utf-8',
+		},
+	});
+};
